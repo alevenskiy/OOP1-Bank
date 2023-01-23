5d76bcd5a00085a9067b1dae0760b9ad59393f55
ale <ppcrn97@gmail.com>
2023-01-23 15:58:51 +0300

    copypast from task 2 and adoptation to task3

--- Task3/MainWindow.xaml.cs
+++ Task3/MainWindow.xaml.cs
@@ -20,9 +20,65 @@ namespace Task3
     /// </summary>
     public partial class MainWindow : Window
     {
+        Consultant consultant;
+        Manager manager;
+        bool isConsultant;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Butt_Download_Click(object sender, RoutedEventArgs e)
+        {
+            if (isConsultant)
+                consultant.LoadClient();
+            else
+                manager.LoadClient();
+        }
+
+        private void Butt_Save_Click(object sender, RoutedEventArgs e)
+        {
+            if (isConsultant)
+            {
+                if (consultant.SaveClient())
+                {
+                    MessageBox.Show("Data saved", "Save Completed", MessageBoxButton.OK);
+                }
+            }
+            else
+            {
+                if (manager.SaveClient())
+                {
+                    MessageBox.Show("Data saved", "Save Completed", MessageBoxButton.OK);
+                }
+            }
+        }
+
+        private void ComboBox_Position_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (ComboBox_Position.SelectedIndex == 0)
+            {
+                if (consultant == null)
+                    consultant = new Consultant(this);
+                isConsultant = true;
+
+                //this.text_surname.Focusable = false;
+                //this.text_name.Focusable = false;
+                //this.text_secondname.Focusable = false;
+                //this.text_passport.Focusable = false;
+            }
+            else
+            {
+                if (manager == null)
+                    manager = new Manager(this);
+                isConsultant = false;
+
+                //this.text_surname.Focusable = true;
+                //this.text_name.Focusable = true;
+                //this.text_secondname.Focusable = true;
+                //this.text_passport.Focusable = true;
+            }
         }
     }
 }
---
+++ Task3/Manager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    internal class Manager : Consultant
+    {
+        public Manager(MainWindow mainWindow) : base(mainWindow)
+        {
+        }
+
+        public void SurnameChange(string surname)
+        {
+            client.surname = surname;
+        }
+        public void NameChange(string name)
+        {
+            client.name = name;
+        }
+        public void SecondnameChange(string secondname)
+        {
+            client.secondName = secondname;
+        }
+        public void PassportChange(string passport)
+        {
+            client.passportNumber = passport;
+        }
+    }
+}
